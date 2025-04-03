9c65fb1396229ceb995e8df8eb06aefb09bc5b9c
sdcautomations <sdcautomations@gmail.com>
2025-04-03 22:10:01 +0200

    chore: add colors enum and declare scss file types

---
+++ theme/colors.ts
@@ -0,0 +1,9 @@
+enum Colors {
+  "backgroundGrey" = "#131616",
+  "lightBackgroundGrey" = "#1B2020",
+  "primaryColor" = "#46B6B5",
+  "textGrey" = "#737F7F",
+  "textCopy" = "#DBF0F0",
+  "chartsAlt1" = "#E2C061",
+  "chartsAlt2" = "#B467DO",
+}
