03f71ad8d01603fe18262863f406d542df8010c7
sdcautomations <sdcautomations@gmail.com>
2025-04-06 20:50:57 +0200

    feat: add Ubuntu font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,31 @@
 import { Stack } from "expo-router";
+import {
+  Ubuntu_400Regular,
+  Ubuntu_500Medium,
+  Ubuntu_700Bold,
+  useFonts,
+} from "@expo-google-fonts/ubuntu";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && error) {
+    return null;
+  }
+
   return <Stack />;
 }
