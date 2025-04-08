3e4d6f5b92e0c18bb0bd5d6cb9e10763a15b3019
sdcautomations <sdcautomations@gmail.com>
2025-04-08 20:08:23 +0200

    refactor: remove unnecessary code and add TextStylesInterface

--- theme/textstyles.ts
+++ theme/textstyles.ts
@@ -4,11 +4,15 @@ import { Colors } from "./colors";
 const REGULAR_FONT = "Ubuntu_400Regular";
 const BOLD_FONT = "Ubuntu_700Bold";
 
+export interface TextStylesInterface {
+  fontFamily: string;
+  fontSize: number;
+}
+
 export const TextStyles = StyleSheet.create({
   copySmall: {
     fontFamily: REGULAR_FONT,
     fontSize: 14,
-    fontWeight: 400,
   },
   copy: {
     fontFamily: REGULAR_FONT,
